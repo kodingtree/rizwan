0814bf648292ee91cb10793bcf2e518b0d6e120a
Rizwan Shaikh <shaikhrizwanali123@gmail.com>
2021-03-05 23:49:36 +0530

    finished

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// This is our JavaScrpt file
+
+var currDate = Date().toLocaleString();
+// var currDate = time
+
+// var textMain = "This file is sent using the JavaScript"
+var textMain = ('So our current time is ' +currDate )   
+
+var text2 = "<br> <br>So this is our new string which is been shown by the JavaScript file."
+
+// document.write(textMain)
