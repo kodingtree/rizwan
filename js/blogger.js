600aaab49c3ec696be0f0def57762e7918eeca13
Rizwan Shaikh <69621612+kodingtree@users.noreply.github.com>
2021-07-12 20:03:42 +0530

    Update blogger.js

--- js/blogger.js
+++ js/blogger.js
@@ -1,2 +1,4 @@
 var ktr = document.getElementById("mycontent");
-ktr.style.setProperty("display", "none", "important")
+//ktr.style.setProperty("display", "none", "important")
+ktr.style.display = "none";
+
